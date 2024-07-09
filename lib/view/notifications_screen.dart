28aa676ff513a9fab946ac4bc3e87789f323b14d
Neyris Bohorquez <113575840+NeyrisBH@users.noreply.github.com>
2024-07-09 14:50:02 -0500

    notify random comic

--- lib/view/notifications_screen.dart
+++ lib/view/notifications_screen.dart
@@ -3,7 +3,9 @@ import 'dart:math';
 import 'package:app_cf_marvel/main_store/main_state.dart';
 import 'package:app_cf_marvel/model/comics_model.dart';
 import 'package:app_cf_marvel/services/notifications_service.dart';
+import 'package:app_cf_marvel/view_model/comics/comics_state.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:provider/provider.dart';
 
 class NotificationsScreen extends StatefulWidget {
@@ -17,113 +19,133 @@ class NotificationsScreenState extends State<NotificationsScreen> {
   bool isSelectedHourly = false;
   bool isSelectedDaily = false;
   bool isSelectedWeekly = false;
-  
+  late ComicsState comicsState;
 
   @override
   void initState() {
-    NotificationsService.askForNotificationPermission();
     super.initState();
+    NotificationsService.askForNotificationPermission();
+    comicsState = ComicsState(main: MainState());
+    comicsState.fetchComics();
   }
 
   String getRandomComicTitle(List<ComicModel> comicsList) {
-  if (comicsList.isEmpty) {
-    return 'No hay cómics disponibles';
+    if (comicsList.isEmpty) {
+      return 'No hay cómics disponibles';
+    }
+    final random = Random();
+    final randomIndex = random.nextInt(comicsList.length);
+    return comicsList[randomIndex].title;
   }
-  final random = Random();
-  final randomIndex = random.nextInt(comicsList.length);
-  return comicsList[randomIndex].title;
-}
 
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     MainState mainState = Provider.of<MainState>(context);
     var userState = mainState.userState;
-    var comicState = mainState.comicsState;
 
-    return Scaffold(
-      body: SizedBox(
-        height: size.height,
-        width: size.width,
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              buildNotificationRow(
-                'Recibir notificaciones cada hora',
-                isSelectedHourly,
-                (value) {
-                  setState(() {
-                    isSelectedHourly = value;
-                  });
-                  if (isSelectedHourly) {
-                    NotificationsService.sendPeriodicNotification(
-                      title: 'Recurrencia horaria',
-                      body: 'Cuerpo del mensaje',
-                      playload: 'playload',
-                      id: 0,
-                    );
-                  } else {
-                    NotificationsService.cancelPeriodicNotification(id: 0);
-                  }
-                },
-              ),
-              buildNotificationRow(
-                'Recibir notificaciones cada día',
-                isSelectedDaily,
-                (value) {
-                  setState(() {
-                    isSelectedDaily = value;
-                  });
-                  if (isSelectedDaily) {
-                    NotificationsService.sendPeriodicNotificationDay(
-                      title: 'Recurrencia diaria',
-                      body: 'Cuerpo del mensaje',
-                      playload: 'playload',
-                      id: 1,
-                    );
-                  } else {
-                    NotificationsService.cancelPeriodicNotification(id: 1);
-                  }
-                },
-              ),
-              buildNotificationRow(
-                'Recibir notificaciones cada semana',
-                isSelectedWeekly,
-                (value) {
-                  setState(() {
-                    isSelectedWeekly = value;
-                  });
-                  if (isSelectedWeekly) {
-                    NotificationsService.sendPeriodicNotificationWeek(
-                      title: 'Recurrencia semanal',
-                      body: 'Cuerpo del mensaje',
-                      playload: 'playload',
-                      id: 2,
-                    );
-                  } else {
-                    NotificationsService.cancelPeriodicNotification(id: 2);
-                  }
-                },
-              ),
-              const SizedBox(height: 20),
-              Center(
-                child: ElevatedButton(
-                  onPressed: () {
-                    NotificationsService.sendInstantNotification(
-                      title: '@${userState.user.value.nickname}, es un buen día para disfrutar de Marvel',
-                      body: 'Ya conocias este comic:${getRandomComicTitle(comicState.comicsList)}',
-                      playload: 'Playload',
-                    );
-                  },
-                  child: const Text('Recibir un detalle'),
+    return Observer(
+      builder: (_) {
+        if (comicsState.isLoading.value) {
+          return const Center(child: CircularProgressIndicator());
+        } else {
+          return Scaffold(
+            body: SizedBox(
+              height: size.height,
+              width: size.width,
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    buildNotificationRow(
+                      'Recibir notificaciones cada hora',
+                      isSelectedHourly,
+                      (value) {
+                        setState(() {
+                          isSelectedHourly = value;
+                        });
+                        if (isSelectedHourly) {
+                          NotificationsService.sendPeriodicNotification(
+                            title: 'Recurrencia horaria',
+                            body: 'Cuerpo del mensaje',
+                            playload: 'playload',
+                            id: 0,
+                          );
+                        } else {
+                          NotificationsService.cancelPeriodicNotification(
+                              id: 0);
+                        }
+                      },
+                    ),
+                    buildNotificationRow(
+                      'Recibir notificaciones cada día',
+                      isSelectedDaily,
+                      (value) {
+                        setState(() {
+                          isSelectedDaily = value;
+                        });
+                        if (isSelectedDaily) {
+                          NotificationsService.sendPeriodicNotificationDay(
+                            title: 'Recurrencia diaria',
+                            body: 'Cuerpo del mensaje',
+                            playload: 'playload',
+                            id: 1,
+                          );
+                        } else {
+                          NotificationsService.cancelPeriodicNotification(
+                              id: 1);
+                        }
+                      },
+                    ),
+                    buildNotificationRow(
+                      'Recibir notificaciones cada semana',
+                      isSelectedWeekly,
+                      (value) {
+                        setState(() {
+                          isSelectedWeekly = value;
+                        });
+                        if (isSelectedWeekly) {
+                          NotificationsService.sendPeriodicNotificationWeek(
+                            title: 'Recurrencia semanal',
+                            body: 'Cuerpo del mensaje',
+                            playload: 'playload',
+                            id: 2,
+                          );
+                        } else {
+                          NotificationsService.cancelPeriodicNotification(
+                              id: 2);
+                        }
+                      },
+                    ),
+                    const SizedBox(height: 20),
+                    Center(
+                      child: ElevatedButton(
+                        onPressed: () {
+                          if (comicsState.comicsList.isNotEmpty) {
+                            NotificationsService.sendInstantNotification(
+                              title: '@${userState.user.value.nickname}, es un buen día para disfrutar de Marvel',
+                              body: '¿Ya conocías este cómic: ${getRandomComicTitle(comicsState.comicsList)}?',
+                              playload: 'Playload',
+                            );
+                          } else {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                content: Text('No hay cómics disponibles para sugerir.'),
+                              ),
+                            );
+                          }
+                        },
+                        child: const Text('Recibir sugerencia'),
+                      ),
+                    ),
+                  ],
                 ),
-              )
-            ],
-          ),
-        ),
-      ),
+              ),
+            ),
+          );
+        }
+      },
     );
   }
 
